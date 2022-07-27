11eee91d946fcd9e4ee04f853fa12fe0154b6071
Michał Chowaniec <chowaniecmichal96@gmail.com>
2022-07-27 14:13:24 +0200

    dodanie funkcjonalności logowania, sprzątanie kodu

--- src/main/java/AccountDetails.java
+++ src/main/java/AccountDetails.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 public class AccountDetails {
-    boolean loggedStatus;//status zalogowania
-    int Lp = 1; //liczba zarejestrowanych użytkowników
+    boolean loggedStatus = false;//status zalogowania
+    int Lp = 0; //liczba zarejestrowanych użytkowników
     String name; //podręczna pamięć nazwy
-    String namemem[] = new String[99]; //baza nazw
+    String[] namemem = new String[99]; //baza nazw
     int accountNumber; //podręczna pamięć numeru konta
-    int accountNumbermem[] = new int[99]; //baza numerów konta
+    int[] accountNumbermem = new int[99]; //baza numerów konta
     String login; //podręczna pamięć loginów
-    String loginmem[] = new String[99];//baza loginów
+    String[] loginmem = new String[99];//baza loginów
     double balance;//podręczna pamięć salda
-    double balancemem[] = new double[99];//baza sald
+    double[] balancemem = new double[99];//baza sald
     Scanner scanner = new Scanner(System.in);//funkcja skanera
         public void createAccount(){
             System.out.println("enter your account name");
@@ -25,20 +25,34 @@ public class AccountDetails {
         System.out.println("enter your account number");
         accountNumber = scanner.nextInt();
         System.out.println("enter your login");
-        login = scanner.next();
-       if((accountNumber == accountNumbermem[accountNumber-1])&(login == loginmem[accountNumber-1])){
-            name = namemem[accountNumber-1];
-            balance = balancemem[accountNumber-1];
-            loggedStatus = true;
-        }else{
-            loggedStatus = false;
-        }
+        login = (scanner.next());
+        //memorycheck
+            for (int z = 0; z < 99; z++)
+            {
+                if ((login.equals(loginmem[z]))&(login.equals(loginmem[accountNumber-1])))
+                {
+                    loggedStatus = true;
+                    break;
+                }
+                else
+                {
+                    loggedStatus = false;
+                }
+            }
     }
     public void cashin(){
-        long deposit = 0;
+        long deposit;
         System.out.println("enter amount of money you want to deposit");
         deposit = scanner.nextLong();
         balance = balance + deposit;
+    }
+    public void DownloadUserData()
+    {
+        accountNumber = accountNumbermem[accountNumber-1];
+        name = namemem[accountNumber-1];
+        login = loginmem[accountNumber-1];
+        balance = balancemem[accountNumber-1];
+        System.out.println("hello "+name);
     }
     public void UpdateUserData(){
             accountNumbermem[accountNumber-1] = accountNumber;
@@ -48,7 +62,7 @@ public class AccountDetails {
         System.out.println("update complete");
     }
     public void cashout(){
-        long amount = 0;
+        long amount;
         System.out.println("enter amount of money you want to withdraw");
         amount = scanner.nextLong();
         if(balance<amount){
--- src/main/java/BankingApp.java
+++ src/main/java/BankingApp.java
@@ -2,77 +2,76 @@ import java.util.Scanner;
 
 public class BankingApp {
     public static void main(String[] args) {
+        boolean loginstatus;
         boolean exit = false; //status kliknięcia wyjścia
-        boolean block = false; //bugfix, aby działał 3. exit
-        boolean loginstatus = false;  //status "czy jest zalogowany?
         Scanner scanner = new Scanner(System.in);//zdefiniowanie skanowania
         AccountDetails account = new AccountDetails();
         do {
             do {
-                System.out.println("Hello in our bank\n" +
-                        "1. Login\n" +
-                        "2. Create new acount\n" +
-                        "3. Exit");
+                loginstatus = false;
+                System.out.println("""
+                        Hello in our bank
+                        1. Login
+                        2. Create new acount
+                        3. Exit""");
                 int z = scanner.nextInt();
                 switch (z) {
-                    case 1:
+                    case 1 -> {
                         account.loginIntoExistingAcc();
-                        if (account.loggedStatus == true) {
-                            account.UpdateUserData();
-                            System.out.println("welcome!");
-                            loginstatus = true;
+                        loginstatus = account.loggedStatus;
+                        if (loginstatus) {
+                            System.out.println("logging in succesfull");
+                            account.DownloadUserData();
                         }
-                        else{
-                            System.out.println("wrong login or account number, please try again");
-                            loginstatus = false;
+                        else
+                        {
+                            System.out.println("bad account number or login do not match");
                         }
-                        break;
-                    case 2:
+                    }
+                    case 2 -> {
                         account.createAccount();
                         account.UpdateUserData();
                         loginstatus = true;
-                        break;
-                    case 3:
-                        exit = true;
-                        block = true;
-                        break;
-                }
-            } while (loginstatus == false & exit == false);
-            int inputNumber;
-            if (block == false) {
-                do {
-                    System.out.println("\nWhat do you want to do?\n" +
-                            "1. Show account info\n" +
-                            "2. Cash deposit\n" +
-                            "3. Cash withdrawal\n" +
-                            "4. Log out\n" +
-                            "5. Exit");
-                    inputNumber = scanner.nextInt();
-                    switch (inputNumber) {
-                        case 1:
-                            account.showAccount();
-                            break;
-                        case 2:
-                            account.cashin();
-                            account.UpdateUserData();
-                            break;
-                        case 3:
-                            account.cashout();
-                            account.UpdateUserData();
-                            break;
-                        case 4:
-                            account.UpdateUserData();
-                            loginstatus = false;
-                            break;
-                        case 5:
-                            exit = true;
-                            break;
                     }
-                }while (inputNumber != 5 & inputNumber != 4);
-            }
+                    case 3 -> {
+                        exit = true;
+                        loginstatus = true;
+                    }
+                }
+            }while(!loginstatus);
+            if(!exit)
+            {
+                    do {
+                        int inputNumber;
+                        System.out.println("""
 
-        }
-        while (exit == false);
+                                What do you want to do?
+                                1. Show account info
+                                2. Cash deposit
+                                3. Cash withdrawal
+                                4. Log out
+                                5. Exit""");
+                        inputNumber = scanner.nextInt();
+                        switch (inputNumber) {
+                            case 1 -> account.showAccount();
+                            case 2 -> {
+                                account.cashin();
+                                account.UpdateUserData();
+                            }
+                            case 3 -> {
+                                account.cashout();
+                                account.UpdateUserData();
+                            }
+                            case 4 -> {
+                                account.UpdateUserData();
+                                loginstatus = false;
+                            }
+                            case 5 -> exit = true;
+                        }
+
+                    } while (loginstatus);
+                }
+        } while (!exit);
     }
 }
 
